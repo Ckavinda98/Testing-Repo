c98af8d3a42fcd474934a9a98734806e3a9b1ce6
Ckavinda98 <Ckavinda57@gmail.com>
2024-05-08 10:30:22 +0530

    testing

--- test.js
+++ test.js
@@ -1,32 +1,32 @@
 let array = ["HelloWorld"];
 
 function countLetters(array) {
-    // Assuming array has only one string element
-    let str = array[0];
-    let lowercaseCount = 0;
-    let uppercaseCount = 0;
+  // Assuming array has only one string element
+  let str = array[0];
+  let lowercaseCount = 0;
+  let uppercaseCount = 0;
 
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] >= 'a' && str[i] <= 'z') {
-            lowercaseCount++;
-        } else if (str[i] >= 'A' && str[i] <= 'Z') {
-            uppercaseCount++;
-        }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] >= "a" && str[i] <= "z") {
+      lowercaseCount++;
+    } else if (str[i] >= "A" && str[i] <= "Z") {
+      uppercaseCount++;
     }
+  }
 
-    return { lowercaseCount, uppercaseCount };
+  return { lowercaseCount, uppercaseCount };
 }
-
+gdgdgjdjgcjgcjggjc;
 function compareCounts(array) {
-    const { lowercaseCount, uppercaseCount } = countLetters(array);
-    
-    if (lowercaseCount === uppercaseCount) {
-        return "Equal";
-    } else if (lowercaseCount > uppercaseCount) {
-        return "More lowercase letters";
-    } else {
-        return "More uppercase letters";
-    }
+  const { lowercaseCount, uppercaseCount } = countLetters(array);
+
+  if (lowercaseCount === uppercaseCount) {
+    return "Equal";
+  } else if (lowercaseCount > uppercaseCount) {
+    return "More lowercase letters";
+  } else {
+    return "More uppercase letters";
+  }
 }
 
 console.log(countLetters(array));
